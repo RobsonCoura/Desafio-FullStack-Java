b95430624c9de88048c7999ad898eb88e7b690b9
Robson Coura <robson_coura@yahoo.com.br>
2023-08-30 10:58:05 -0300

    Indentação de código na camada PaisController

--- Backend/src/main/java/br/com/juridico/totvs/fullstack/Backend/controller/PaisController.java
+++ Backend/src/main/java/br/com/juridico/totvs/fullstack/Backend/controller/PaisController.java
@@ -1,15 +1,10 @@
 package br.com.juridico.totvs.fullstack.Backend.controller;
 
-import br.com.juridico.totvs.fullstack.Backend.domain.Pais;
 import br.com.juridico.totvs.fullstack.Backend.service.PaisService;
-import br.com.juridico.totvs.fullstack.Backend.service.PaisServiceImpl;
 import br.com.juridico.totvs.fullstack.Backend.service.dto.PaisCreateUpdateDTO;
 import br.com.juridico.totvs.fullstack.Backend.service.dto.PaisDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -19,40 +14,40 @@ import java.util.List;
 public class PaisController {
     private final PaisService paisService;
 
-    public PaisController(PaisService paisService){
+    public PaisController(PaisService paisService) {
         this.paisService = paisService;
     }
 
     @PostMapping
-    @ResponseStatus( HttpStatus.CREATED )
-    public PaisDTO create(@RequestBody PaisCreateUpdateDTO paisCreateUpdateDTO){
+    @ResponseStatus(HttpStatus.CREATED)
+    public PaisDTO create(@RequestBody PaisCreateUpdateDTO paisCreateUpdateDTO) {
         return this.paisService.create(paisCreateUpdateDTO);
     }
 
     @GetMapping
-    public List<PaisDTO> getAll(){
+    public List<PaisDTO> getAll() {
         return this.paisService.getAllPais();
     }
 
     @GetMapping("{continente}/continente")
-    public List<PaisDTO> getPaisByContinente(@PathVariable String continente){
+    public List<PaisDTO> getPaisByContinente(@PathVariable String continente) {
         return this.paisService.getPaisByContinente(continente);
     }
 
     @GetMapping("{idPais}")
-    public PaisDTO getPaisById(@PathVariable Long idPais){
+    public PaisDTO getPaisById(@PathVariable Long idPais) {
         return this.paisService.getPaisbyId(idPais);
     }
 
     @DeleteMapping("{idPais}")
-    @ResponseStatus( HttpStatus.NO_CONTENT )
-    public void delete(@PathVariable Long idPais){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long idPais) {
         this.paisService.delete(idPais);
     }
 
     @PutMapping("{idPais}")
     public PaisDTO update(@PathVariable Long idPais,
-                          @RequestBody PaisCreateUpdateDTO paisCreateUpdateDTO ){
+                          @RequestBody PaisCreateUpdateDTO paisCreateUpdateDTO) {
         return this.paisService.update(idPais, paisCreateUpdateDTO);
     }
 }
