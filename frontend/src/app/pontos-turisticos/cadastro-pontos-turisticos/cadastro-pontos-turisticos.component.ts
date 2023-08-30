b7b87dc05a99a3da81f6d0f8dba36ae28cd54bd4
Robson Coura <robson_coura@yahoo.com.br>
2023-08-30 11:03:36 -0300

    Integração com o back-end e implementação lógica de navegação

--- frontend/src/app/pontos-turisticos/cadastro-pontos-turisticos/cadastro-pontos-turisticos.component.ts
+++ frontend/src/app/pontos-turisticos/cadastro-pontos-turisticos/cadastro-pontos-turisticos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PoNotificationService } from '@po-ui/ng-components';
+import { HttpService } from 'src/app/service/http-service.service';
+import { triggerFormValidators } from 'src/app/shared/util';
 
 @Component({
   selector: 'app-cadastro-pontos-turisticos',
@@ -7,9 +12,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroPontosTuristicosComponent implements OnInit {
 
-  constructor() { }
+  idPontoTuristico: string;
+	formPontoTuristico: FormGroup;
+	title: string = "Novo cadastro de Ponto Turístico"
+	constructor(private formBuilder: FormBuilder,
+		private poNotification: PoNotificationService,
+		private route: ActivatedRoute,
+		private router: Router,
+		private http: HttpService
+		) {
 
-  ngOnInit(): void {
-  }
+		this.formPontoTuristico = this.formBuilder.group({
+			nome: ['', Validators.compose([Validators.required])],
+			paisId: ['', Validators.compose([Validators.required])],
+			cidade: ['', Validators.compose([Validators.required])],
+			estacaoVisita: [, Validators.compose([Validators.required])]
+		})
+	}
 
+	ngOnInit(): void {
+		this.idPontoTuristico = this.route.snapshot.paramMap.get('idPontoTuristico');
+		if (this.idPontoTuristico !== null && this.idPontoTuristico !== ""){
+			this.buscaDadosPontoTuristico()
+			this.title = "Alteração do Ponto Turístico"
+		}
+	}
+
+	salvar(){
+		if (this.validarRegistro()){
+			if (this.idPontoTuristico === null || this.idPontoTuristico === ""){
+				this.enviarPost()
+			} else {
+				this.enviarPut()
+			}
+		} else {
+			this.poNotification.error("Preencha todos os campos antes de salvar as alterações!")
+		}
+	}
+
+	voltar(){
+		this.router.navigate(['/ponto-turistico'], { relativeTo: this.route })
+	}
+
+	validarRegistro(): boolean{
+		return this.formPontoTuristico.valid;
+	}
+
+	enviarPost(){
+		this.http.post('pontoturistico',this.formPontoTuristico.value).subscribe({
+			next:(resposta) => {
+				this.poNotification.success("Registro criado com sucesso!");
+				this.voltar();
+			},
+			error:(erro) => {
+				this.poNotification.error(erro)
+			},
+		})
+	}
+
+	enviarPut(){
+		this.http.put('pontoturistico/' + this.idPontoTuristico,this.formPontoTuristico.value).subscribe({
+			next:(resposta) => {
+				this.poNotification.success("Registro atualizado com sucesso!");
+				this.voltar();
+			},
+			error:(erro) => {
+				this.poNotification.error(erro)
+			},
+		})
+	}
+
+	buscaDadosPontoTuristico(){
+		this.http.get('pontoturistico/' + this.idPontoTuristico).subscribe({
+			next: (resposta)=>{
+				this.formPontoTuristico.patchValue({
+	        id: resposta.id,
+	        paisId: resposta.paisId,
+	        cidade: resposta.cidade,
+	        nome: resposta.nome,
+	        estacaoVisita: resposta.estacaoVisita
+				})
+			},
+			error: (erro)=>{
+				this.poNotification.error(erro)
+			}
+		})
+	}
 }
+
--- frontend/src/app/pontos-turisticos/pontos-turisticos.component.ts
+++ frontend/src/app/pontos-turisticos/pontos-turisticos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PoNotificationService, PoTableAction, PoTableColumn } from '@po-ui/ng-components';
+import { map, tap } from 'rxjs';
+import { HttpService } from '../service/http-service.service';
 
 @Component({
   selector: 'app-pontos-turisticos',
@@ -7,9 +11,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PontosTuristicosComponent implements OnInit {
 
-  constructor() { }
+  lsActions: Array<PoTableAction> = this.carregarActions();
+	lsColumns: Array<PoTableColumn> = this.carregarColunas();
+	lsPontosTuristicos: Array<PontosTuristicos> = []
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private httpService: HttpService,
+		private poNotification: PoNotificationService,
+		private router: Router,
+		private activatedRoute: ActivatedRoute) { }
 
+	ngOnInit(): void {
+		this.carregarPontosTuristicos()
+	}
+
+	carregarActions(): Array<PoTableAction> {
+		return [
+			{
+				label: 'Editar',
+				icon: 'po-icon-edit',
+				action: (row: PontosTuristicos)=>{ this.navegarParaCadastro(row.id) }
+			},
+			{
+				label: 'Excluir',
+				icon: 'po-icon-delete',
+				action: (row: PontosTuristicos)=>{ this.deletarCadastro(row.id) }
+			}
+		]
+	}
+
+	deletarCadastro(id: string): void {
+		this.httpService.delete('pontoturistico/' + id).subscribe({
+			next: (response)=>{
+				this.poNotification.success('Registro excluido com sucesso!');
+				this.carregarPontosTuristicos();
+			},
+			error: (error)=>{
+				this.poNotification.error(error);
+			}
+		})
+	}
+
+	navegarParaCadastro(codigoPontoTuristico: string = ""){
+		console.log(this.router.url)
+this.router.navigate(['cadastro', codigoPontoTuristico], { relativeTo: this.activatedRoute });
+	}
+
+	carregarPontosTuristicos(){
+		return this.httpService.get('pontoturistico').subscribe({
+			next: (resposta)=>{
+			console.log(resposta)
+				let registros: Array<PontosTuristicos> = []
+				resposta.forEach(item => {
+					let novoPontoTuristico: PontosTuristicos = {
+							id: item.id,
+            	paisId: item.paisId,
+            	cidade: item.cidade,
+            	nome: item.nome,
+            	estacaoVisita: item.estacaoVisita
+					}
+					registros.push(novoPontoTuristico);
+				});
+
+				this.lsPontosTuristicos = [...registros]
+			}
+		})
+	}
+
+	carregarColunas(): Array<PoTableColumn>{
+		return [
+			{
+				property: 'paisId',
+				label: 'País'
+			},
+			{
+				property: 'cidade',
+				label: 'Cidade',
+			},
+			{
+				property: 'nome',
+				label: 'Nome'
+			},
+			{
+				property: 'estacaoVisita',
+				label: 'Estação Visita'
+			}
+		]
+	}
 }
+
+interface PontosTuristicos{
+	id: string,
+	paisId: string,
+	cidade: string,
+	nome: string,
+	estacaoVisita: string
+}
+
