ae5ae73fed5bca31caf4ca368453d6bb66276006
Robson Coura <robson_coura@yahoo.com.br>
2023-09-01 16:52:36 -0300

    Integração com o back-end e implementação lógica de navegação

--- frontend/src/app/comentarios/cadastro-comentario/cadastro-comentario.component.ts
+++ frontend/src/app/comentarios/cadastro-comentario/cadastro-comentario.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PoNotificationService } from '@po-ui/ng-components';
+import { HttpService } from 'src/app/service/http-service.service';
+import { triggerFormValidators } from 'src/app/shared/util';
 
 @Component({
   selector: 'app-cadastro-comentario',
@@ -7,9 +12,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComentarioComponent implements OnInit {
 
-  constructor() { }
+  idComentario: string;
+	formComentario: FormGroup;
+	title: string = "Novo cadastro de Comentário"
+	constructor(private formBuilder: FormBuilder,
+		private poNotification: PoNotificationService,
+		private route: ActivatedRoute,
+		private router: Router,
+		private http: HttpService
+		) {
 
-  ngOnInit(): void {
-  }
+		this.formComentario = this.formBuilder.group({
+			nome: ['', Validators.compose([Validators.required])],
+			paisId: ['', Validators.compose([Validators.required])],
+			cidade: ['', Validators.compose([Validators.required])],
+			estacaoVisita: [, Validators.compose([Validators.required])]
+		})
+	}
 
+	ngOnInit(): void {
+		this.idComentario = this.route.snapshot.paramMap.get('idComentario');
+		if (this.idComentario !== null && this.idComentario !== ""){
+			this.buscaDadosComentario()
+			this.title = "Alteração do Comentário"
+		}
+	}
+
+	salvar(){
+		if (this.validarRegistro()){
+			if (this.idComentario === null || this.idComentario === ""){
+				this.enviarPost()
+			} else {
+				this.enviarPut()
+			}
+		} else {
+			this.poNotification.error("Preencha todos os campos antes de salvar as alterações!")
+		}
+	}
+
+	voltar(){
+		this.router.navigate(['/comentario'], { relativeTo: this.route })
+	}
+
+	validarRegistro(): boolean{
+		return this.formComentario.valid;
+	}
+
+	enviarPost(){
+		this.http.post('comentario',this.formComentario.value).subscribe({
+			next:(resposta) => {
+				this.poNotification.success("Registro criado com sucesso!");
+				this.voltar();
+			},
+			error:(erro) => {
+				this.poNotification.error(erro)
+			},
+		})
+	}
+
+	enviarPut(){
+		this.http.put('comentario/' + this.idComentario,this.formComentario.value).subscribe({
+			next:(resposta) => {
+				this.poNotification.success("Registro atualizado com sucesso!");
+				this.voltar();
+			},
+			error:(erro) => {
+				this.poNotification.error(erro)
+			},
+		})
+	}
+
+	buscaDadosComentario(){
+		this.http.get('comentario/' + this.idComentario).subscribe({
+			next: (resposta)=>{
+				this.formComentario.patchValue({
+	        id: resposta.id,
+	        pontoTuristicoId: resposta.pontoTuristicoId,
+	        descricao: resposta.descricao,
+	        nomeDoAutor: resposta.nomeDoAutor,
+	        dataDaCriacao: resposta.dataDaCriacao
+				})
+			},
+			error: (erro)=>{
+				this.poNotification.error(erro)
+			}
+		})
+	}
 }
+
